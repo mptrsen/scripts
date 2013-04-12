a1cc8919a3469d092591afc2237eeb6119c73c10
Malte Petersen <mptrsen@uni-bonn.de>
2013-04-12 15:58:53 +0200

    More virusblast

--- virusblast.pl
+++ virusblast.pl
@@ -14,26 +14,48 @@ my %opt = ();
 GetOptions( \%opt,
 	'db=s',
 	'blast-location=s',
+	'blast-threads=i',
 	'outdir=s',
+	'verbose|v',
 );
 
 $opt{'db'}             //= '/share/pool/nr/2013-03-01/nr';
-$opt{'blast-location'} //= '/share/apps/blastx_2.2.26+';
+$opt{'blast-location'} //= '/share/apps/blastp_2.2.26+';
+$opt{'blastdbcmd-location'} //= '/share/apps/blastdbcmd_2.2.26+';
+$opt{'blast-threads'}  //= 1;
 $opt{'outdir'}         //= '.';
 my @queries = @ARGV;
+
+my $blastp = File::Spec->catfile($opt{'blast-location'});
+my $blastdbcmd = File::Spec->catfile($opt{'blastdbcmd-location'});
+my $verbose = $opt{'verbose'};
+
 foreach my $queryfile (@queries) {
+
 	my $blastofn = File::Spec->catfile($opt{'outdir'}, basename($queryfile . '.blastout'));
-	my @blastcmd = qq($opt{'blast-location'} -db $opt{'db'} -query $queryfile -outfmt '7 sseqid sgi sacc sallseqid sallgi sallacc' -out $blastofn);
-	system(@blastcmd) and die "Fatal: BLAST failed: $!\n";
+	print "Using BLASTP output file $blastofn\n" if $verbose;
+
+	my @blastcmd = qq( $blastp -num_threads $opt{'blast-threads'} -db $opt{'db'} -query $queryfile -outfmt '7 sseqid sgi sacc sallseqid sallgi sallacc' -out $blastofn);
+
+	print "Executing '@blastcmd'\n" if $verbose;
+	system(@blastcmd) and die "Fatal: $opt{'blast-location'} failed: $!\n";
+
 	my $ofh = IO::File->new($blastofn, 'r');
 	my @blastresult = <$ofh>;
-	print @blastresult;
 	@blastresult = grep /^[^#]/, @blastresult;
 	if (scalar @blastresult == 0) {
-		print "No hits obtained for $queryfile\n";
+		print "No BLASTP hits obtained for $queryfile\n";
 		next;
 	}
 	my @fields = split /\s+/, $blastresult[0];
 	my $id = $fields[1];
-	my @blastdbcmdcmd = qq( File::Spec->catfile(basename($opt{'blast-location'}), 'blastdbcmd') -db $opt{'db'} );
+	my $dbofn = File::Spec->catfile($blastofn . '.dbo');
+	my @blastdbcmdcmd = qq( $blastdbcmd -db $opt{'db'} -out $dbofn -entry $id -outfmt '%i is taxid %T: %S (%L)');
+	print "Executing '@blastdbcmdcmd'\n" if $verbose;
+	system(@blastdbcmdcmd) and die "Fatal: blastdbcmd failed: $!\n";
+
 }
+
+print "Done. Exiting.\n";
+
+exit;
