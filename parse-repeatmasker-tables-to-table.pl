da6532835029912ba827fd97b7449ed2e1e39ba2
Malte Petersen <mptrsen@uni-bonn.de>
2017-04-19 14:00:25 +0200

    Update repeatmasker table parser to output proper file names when run on multiple tables

--- parse-repeatmasker-tables-to-table.pl
+++ parse-repeatmasker-tables-to-table.pl
@@ -22,37 +22,37 @@ while (<>) {
 		$genome_size = $1;
 	}
 	elsif (/^Total interspersed repeats:\h*(\d+) bp/) {
-		push @interestinglines, { type => 'Total interspersed repeats', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Total interspersed repeats', value => $1 };
 	}
 	elsif (/^SINEs:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'SINEs', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'SINEs', value => $1 };
 	}
 	elsif (/^LINEs:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'LINEs', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'LINEs', value => $1 };
 	}
 	elsif (/^LTR elements:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'LTR elements', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'LTR elements', value => $1 };
 	}
 	elsif (/^DNA elements:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'DNA elements', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'DNA elements', value => $1 };
 	}
 	elsif (/^Unclassified:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'Unclassified elements', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Unclassified elements', value => $1 };
 	}
 	elsif (/^Small RNA:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'Small RNA', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Small RNA', value => $1 };
 	}
 	elsif (/^Satellites:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'Satellites', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Satellites', value => $1 };
 	}
 	elsif (/^Simple repeats:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'Simple repeats', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Simple repeats', value => $1 };
 	}
 	elsif (/^Low complexity:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { type => 'Low complexity', value => $1 };
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Low complexity', value => $1 };
 	}
 }
 
 foreach my $line (@interestinglines) {
-	print join("\t", $species_name ? $species_name : $file_name, $genome_size, $line->{type}, $line->{value}), "\n";
+	print join("\t", $species_name ? $species_name : $line->{file}, $line->{genome_size}, $line->{type}, $line->{value}), "\n";
 }
