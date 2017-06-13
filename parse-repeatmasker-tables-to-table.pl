18f6c3cb248c181cb8b4d7cc8a4fa5f2bcb1b463
Malte Petersen <mptrsen@uni-bonn.de>
2017-06-13 10:54:36 +0200

    Prettified RM table parser

--- parse-repeatmasker-tables-to-table.pl
+++ parse-repeatmasker-tables-to-table.pl
@@ -5,15 +5,34 @@ use autodie;
 use Getopt::Long;
 
 my $species_name = '';
+my $print_header = 0;
 
 my $opts = GetOptions(
-	'species=s' => \$species_name
+	'species=s' => \$species_name,
+	'print-header|p' => \$print_header,
 ) or die;
 
 my @interestinglines;
 my $genome_size = 0;
 my $file_name = '';
 
+# define the elements I want to collect
+my @interestingelements = (
+	'Total interspersed repeats',
+	'SINEs',
+	'LINEs',
+	'LTR elements',
+	'DNA elements',
+	'Unclassified',
+	'Small RNA',
+	'Satellites',
+	'Simple repeats',
+	'Low complexity',
+);
+
+# go through each input file, picking out file name and total length as well as
+# total repeat coverage also collect the elements defined above by pushing an
+# anonymous hash reference with the data I need to the stack
 while (<>) {
 	if (/^file name:\h*(.+)$/) {
 		$file_name = $1;
@@ -21,38 +40,22 @@ while (<>) {
 	elsif (/^total length:\h+(\d+) bp/) {
 		$genome_size = $1;
 	}
-	elsif (/^Total interspersed repeats:\h*(\d+) bp/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Total interspersed repeats', value => $1 };
+	elsif (/^(Total interspersed repeats):\h*(\d+) bp/) {
+		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => $1, value => $2 };
 	}
-	elsif (/^SINEs:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'SINEs', value => $1 };
-	}
-	elsif (/^LINEs:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'LINEs', value => $1 };
-	}
-	elsif (/^LTR elements:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'LTR elements', value => $1 };
-	}
-	elsif (/^DNA elements:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'DNA elements', value => $1 };
-	}
-	elsif (/^Unclassified:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Unclassified elements', value => $1 };
-	}
-	elsif (/^Small RNA:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Small RNA', value => $1 };
-	}
-	elsif (/^Satellites:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Satellites', value => $1 };
-	}
-	elsif (/^Simple repeats:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Simple repeats', value => $1 };
-	}
-	elsif (/^Low complexity:\h+\d+\h+(\d+) bp\h+/) {
-		push @interestinglines, { file => $file_name, genome_size => $genome_size, type => 'Low complexity', value => $1 };
+	else {
+		foreach my $elem (@interestingelements) {
+			if (/^($elem):\h+\d+\h+(\d+) bp\h+/) {
+				push @interestinglines, { file => $file_name, genome_size => $genome_size, type => $1, value => $2 };
+			}
+		}
 	}
 }
 
+# done collecting, output
+if ($print_header) {
+	print join("\t", 'genome', 'genome size', 'element type', 'element coverage'), "\n";
+}
 foreach my $line (@interestinglines) {
 	print join("\t", $species_name ? $species_name : $line->{file}, $line->{genome_size}, $line->{type}, $line->{value}), "\n";
 }
