90268a06fc66420dc368d92c82d4bbd5155a0e17
Malte Petersen <mptrsen@uni-bonn.de>
2017-07-07 14:18:26 +0200

    Refactored to OO

--- fetch-families-from-repeatmodeler-consensi.pl
+++ fetch-families-from-repeatmodeler-consensi.pl
@@ -14,52 +14,139 @@ use Getopt::Long;
 #
 # Parameters: path to RModeler output directory
 
-my $usage = "Usage: $0 RModeler_dir\n";
+my $usage = "Usage: $0 [--ncpu N] [--outdir Output_dir] RModeler_dir\n";
 
 my $outdir;
-my $opts = GetOptions( "outdir=s" => \$outdir ) or die "Error in command line arguments\n";
+my $ncpu;
+my $rmdir;
+my $opts = GetOptions( "outdir=s" => \$outdir, 'ncpu=i' => \$ncpu, 'rmdir=s' => \$rmdir ) or die "Error in command line arguments\n";
 $outdir //= '.';
+$ncpu   //= 1;
+$rmdir  //= shift @ARGV or die $usage;
 
-my $rmdir = shift or die $usage;
-
+# find and load consensus sequences into memory
+# (even though we only need the headers)
 my $consensi_file = File::Spec->catfile($rmdir, 'consensi.fa.classified');
-
 my $consensus_sequences = Seqload::Fasta::slurp_fasta($consensi_file);
 
 # go through keys, parse round and family information, assign file paths to them
-my $props = { };
-foreach my $header (keys %$consensus_sequences) {
-	$header =~ s/ .+$//; # remove everything after the first part
-	my ($rndfam, $name) = split '#', $header;
-	my ($rnd, $fam) = split '_', $rndfam;
-	$rnd =~ s/^rnd-//; # isolate the number
-	$fam =~ s/^family-//; # isolate the number
-	printf "header: %s\nround: %s\nfamly: %s\nname: %s\n", $header, $rnd, $fam, $name;
-	my $props->{$header} = { 'file' => File::Spec->catfile($rmdir, 'round-' . $rnd, 'family-' . $fam . '.fa'), 'name' => $name };
-	printf "File: %s\n", $header, $props->{$header}->{'file'};
-	print "Making MSA for $header\n";
-	$props->{$header}->{'msa'} = make_alignment($props->{$header});
-	print "Making HMM for $header\n";
-	$props->{$header}->{'hmm'} = make_hmm($props->{$header});
+foreach my $header (sort { $a cmp $b } keys %$consensus_sequences) {
+	my $family = Family->new($rmdir, $header);
+	printf "Making MSA for round %d, family %d (%s) from file %s\n", $family->round(), $family->family(), $family->name(), $family->file();
+	$family->make_alignment( $outdir, $ncpu );
+	printf "MSA file: %s\n", $family->msafile();
+	print "Making HMM\n";
+	$family->make_hmm( $outdir, $ncpu );
+	printf "HMM file: %s\n", $family->hmmfile();
 	print "-------------------\n";
 }
 
+package Family;
+
+use strict;
+use warnings;
+use autodie;
+use File::Basename;
+use File::Spec::Functions;
+use Carp;
+
+sub new {
+	my $class = shift;
+	my $rmdir = shift || croak;
+	my $hdr = shift || croak;
+	my $self = parse_header($hdr);
+	bless $self, $class;
+	$self->file( File::Spec->catfile($rmdir, 'round-' . $self->round(), 'family-' . $self->family() . '.fa') );
+	return $self;
+}
+
+sub parse_header {
+	my $hdr = shift @_;
+	$hdr =~ s/ .+$//; # remove everything after the first part
+	my ($rndfam, $name) = split '#', $hdr;
+	my ($rnd, $fam) = split '_', $rndfam;
+	$rnd =~ s/^rnd-//; # isolate the number
+	$fam =~ s/^family-//; # isolate the number
+	my $data = {
+		'name'   => $name,
+		'round'  => $rnd,
+		'family' => $fam,
+		'header' => $hdr,
+	};
+	return $data;
+}
+
+sub output_directory {
+	my $self = shift;
+	return $self->{'output_directory'} if defined $self->{'output_directory'};
+	$self->{'output_directory'} = shift;
+}
+
+sub file {
+	my $self = shift;
+	return $self->{'file'} if defined $self->{'file'};
+	$self->{'file'} = shift || croak;
+}
+
+sub name {
+	my $self = shift;
+	return $self->{'name'} if defined $self->{'name'};
+	$self->{'name'} = shift || carp;
+}
+
+sub round {
+	my $self = shift;
+	return $self->{'round'} if defined $self->{'round'};
+	$self->{'round'} = shift || carp;
+}
+
+sub family {
+	my $self = shift;
+	return $self->{'family'} if defined $self->{'family'};
+	$self->{'family'} = shift || carp;
+}
+
+sub header {
+	my $self = shift;
+	return $self->{'header'} if defined $self->{'header'};
+	$self->{'header'} = shift || carp;
+}
+
+sub msafile {
+	my $self = shift;
+	return $self->{'msafile'} if defined $self->{'msafile'};
+	$self->{'msafile'} = shift || carp;
+}
+
+sub hmmfile {
+	my $self = shift;
+	return $self->{'hmmfile'} if defined $self->{'hmmfile'};
+	$self->{'hmmfile'} = shift || carp;
+}
+
 sub make_alignment {
-	my $inprops = shift @_;
-	my $inf = $inprops->{'file'};
+	my $self = shift;
+	my $outdir = shift || croak;
+	my $ncpu = shift || croak;
+	my $inf = $self->file();
 	my $outf = catfile($outdir, basename($inf, '.fa') . '.afa');
-	system("linsi '$inf' > '$outf' 2> /dev/null") and die;
-	return $outf;
+	system("linsi --thread $ncpu '$inf' > '$outf' 2> /dev/null") and die;
+	$self->msafile($outf);
+	return $self->msafile();
 }
 
 sub make_hmm {
-	my $inprops = shift @_;
-	my $inf = $inprops->{'msa'};
-	my $name = $inprops->{'name'};
+	my $self = shift @_;
+	my $outdir = shift || croak;
+	my $ncpu = shift || croak;
+	my $inf = $self->msafile();
+	my $name = $self->header();
 	my $outf = catfile($outdir, basename($inf) . '.hmm');
-	system("hmmbuild -n '$name' --informat afa '$outf' '$inf'") and die;
-	return $outf;
+	system("hmmbuild --cpu $ncpu -n '$name' --informat afa '$outf' '$inf' 2> /dev/null") and die;
+	$self->hmmfile($outf);
+	return $self->hmmfile();
 }
+
 
 package Seqload::Fasta;
 use strict;
