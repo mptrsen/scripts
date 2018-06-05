ff40513cb5f8c15ea73b1e41406216fec015e96e
Malte Petersen <mptrsen@uni-bonn.de>
2018-06-05 14:55:36 +0200

    Add strip_tags.py

---
+++ strip_tags.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from html.parser import HTMLParser
+import fileinput
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+html = ""
+
+for line in fileinput.input():
+    html = html + line
+
+print(strip_tags(html))
